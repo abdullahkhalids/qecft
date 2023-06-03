6472f196509efcf579557e43e040ed2dd42ab772
AnsahMohammad <mohdansah10@gmail.com>
2023-06-03 17:50:51 +0530

    updated forum.sh

--- forum.sh
+++ forum.sh
@@ -1,19 +1,19 @@
 #!/bin/bash
 
 # creating the virtual environment
-echo "creating the virtual environment"
-python -m venv envqecft
+echo "Creating the virtual environment"
+python3 -m venv envqecft
 source envqecft/bin/activate
 
 # dependencies
-echo "installing the dependencies"
-pip install -r requirements.txt
+echo "Installing the dependencies"
+pip3 install -r requirements.txt
 
 # Start the Flask server
 echo "Starting Flask server..."
 cd forum 
 export FLASK_APP=forumcreator.py
-flask run &
+flask run --port=8000 &
 sleep 5
 
 # Start the Isso server
