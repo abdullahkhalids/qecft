4600c663d0c0ade38ac5ea9a95cd91c89762d9a9
AnsahMohammad <mohdansah10@gmail.com>
2023-06-05 11:17:28 +0530

    Minor changes in forum

--- forum/forumcreator.py
+++ forum/forumcreator.py
@@ -2,7 +2,7 @@ import os
 from flask import Flask, render_template, request, render_template_string
 from nbconvert.filters import markdown2html
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 
 ISSO_CONFIG = """
 <div id="isso-thread"></div>
@@ -36,7 +36,7 @@ def create_new_page(section_number, task_number, question):
     page_title = f"Section {section_number} Task {task_number}"
     page_contents = markdown2html(question)
     page_filename = f"section_{section_number}_task_{task_number}.html"
-    page_directory = "templates"
+    page_directory = "solutions"
     os.makedirs(page_directory, exist_ok=True)
     
     content = f"<h3>Comments</h3>{ISSO_CONFIG}"
@@ -59,16 +59,15 @@ def create_page():
         section_number = request.form.get("section_number")
         task_number = request.form.get("task_number")
         question = request.form.get("question")
-
         page_filename, markdown_string = create_new_page(section_number, task_number, question)
-        link_output = f"/comments/{page_filename}"
-        return render_template("index.html", markdown_output=markdown_string, link_output=link_output)
+        link_output = f"/solutions/{page_filename}"
+        return render_template("createpage/index.html", markdown_output=markdown_string, link_output=link_output)
 
-    return render_template("index.html")
+    return render_template("createpage/index.html")
 
-@app.route("/comments/<path:name>")
+@app.route("/solutions/<path:name>")
 def view_comments(name):
-    return render_template(name)
+    return render_template(f'solutions/{name}')
 
 if __name__ == '__main__':
     app.run(host='localhost', port=8000)
