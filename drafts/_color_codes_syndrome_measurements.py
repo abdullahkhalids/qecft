bbefba5dd071e33e7c9cdbbeaf092cce80b3ccf6
victor-onofre <vonofre68@gmail.com>
2023-06-10 11:16:01 -0700

    First function for code of distance 3

--- drafts/_color_codes_syndrome_measurements.py
+++ drafts/_color_codes_syndrome_measurements.py
@@ -99,101 +99,52 @@ def vertices_physical_qubits(d):
 
 def _color_codes_syndrome_measurements(d):
     '''
-    This function creates syndrome measurement circuit with surface code of any dimension. 
+    This function creates syndrome measurement circuit with 4.8.8 color code family
 
     Parameter
     -----------
-    col : int > 0
-        The number of qubits in a horizontal line. 
-    
-    row : int > 0
-        The number of qubits in a vertical line.
-    
-        
+    d : int > 0
+        distance
+
     Return
     --------
     stac.Circuit with appended gates
     '''
+    faces, H = vertices_physical_qubits(d)
+
+    circ = stac.Circuit()
+    circ.append_register(stac.QubitRegister('d', 0, len(H.nodes)))
+    circ.append_register(stac.RegisterRegister('s', 0))
+    circ.register[0, 1].append(stac.RegisterRegister('t', 0))
+    circ.register[0, 1].append(stac.RegisterRegister('t', 1))
+
+    for i in range(len(faces)):
+        circ.register[0, 1][0].append(stac.QubitRegister('s', i, 1))
+        circ.register[0, 1][1].append(stac.QubitRegister('s', i, 1))
+
+    qubit_number = list(H.nodes)
+
+    for i in range(len(faces)):
+        for j in range(len(faces[i])):
+            print(qubit_number.index(faces[i][j]), faces[i][j])
+            number = qubit_number.index(faces[i][j])
+            circ.append('CX', (0, 0, number), (0, 1, 0, i, 0))
+        circ.append('MR', (0, 1, 0, i, 0))
+        circ.append('TICK')
+
+    for i in range(len(faces)):
+        print(i)
+        circ.append('H', (0, 1, 1, i, 0))
+        for j in range(len(faces[i])):
+            # print(qubit_number.index(faces[i][j]), faces[i][j])
+            number = qubit_number.index(faces[i][j])
+            circ.append('CX', (0, 1, 1, i, 0), (0, 0, number))
+        circ.append('H', (0, 1, 1, i, 0))
+        circ.append('MR', (0, 1, 1, i, 0))
+        circ.append('TICK')
     
-    surface_code_circ = stac.Circuit()
-    num = row * col
+    return circ
 
-    #determine the number of data points and syndrome points
-    if row % 2 == 0:
-        d_num = s_num = num // 2
-    else:
-        if col % 2 == 0:
-            d_num = s_num = num // 2 
-        else:
-            d_num = (num + 1) // 2
-            s_num = d_num - 1
+_color_codes_syndrome_measurements(3)
 
-    surface_code_circ.append_register(stac.QubitRegister('d', 0, d_num))
-    surface_code_circ.append_register(stac.QubitRegister('s', 0, s_num))
-
-    #assign coordinates
-    surface_code_circ.physical_register.elements = \
-    [stac.PhysicalQubit(col*i+j, (j, i), []) for i in range(row) for j in range(col)]
-
-    surface_code_circ.layout_map = bidict.bidict()
-
-    d_i = 0
-    s_i = 0
-
-    for i in range(row):
-        if i % 2 == 0:
-            for j in range(col):
-                if j % 2 == 0:
-                    surface_code_circ.register[(0,0, d_i)].constituent_register = surface_code_circ.physical_register.elements[col*i+j]
-                    surface_code_circ.layout_map[(0,0,d_i)] = (j, i)
-                    d_i += 1
-                    
-                else:
-                    surface_code_circ.register[(0,1,s_i)].constituent_register = surface_code_circ.physical_register.elements[col*i+j]
-                    surface_code_circ.layout_map[(0,1,s_i)] = (j, i)
-                    s_i += 1
-        
-        else:
-            for j in range(col):
-                if j % 2 != 0:
-                    surface_code_circ.register[(0,0, d_i)].constituent_register = surface_code_circ.physical_register.elements[col*i+j]
-                    surface_code_circ.layout_map[(0,0,d_i)] = (j, i)
-                    d_i += 1
-
-                else:
-                    surface_code_circ.register[(0,1,s_i)].constituent_register = surface_code_circ.physical_register.elements[col*i+j]
-                    surface_code_circ.layout_map[(0,1,s_i)] = (j, i)
-                    s_i += 1
-
-    #create circuit
-    surface_code_circ.clear()
-
-    for qubit in surface_code_circ.register[(0,1)].qubits():
-
-        s_coordinate = qubit.constituent_register.coordinates
-
-        d_1 = (qubit.constituent_register.coordinates[0], qubit.constituent_register.coordinates[1]+1)
-        d_2 = (qubit.constituent_register.coordinates[0]-1, qubit.constituent_register.coordinates[1])
-        d_3 = (qubit.constituent_register.coordinates[0]+1, qubit.constituent_register.coordinates[1])
-        d_4 = (qubit.constituent_register.coordinates[0], qubit.constituent_register.coordinates[1]-1)
-
-        if s_coordinate[1] % 2 == 0: #Z
-
-            for i in [d_1, d_2, d_3, d_4]:
-                if -1 < i[0] < col and -1 < i[1] < row:
-                    surface_code_circ.geo_append('CX', i, s_coordinate)
-
-            surface_code_circ.geo_append('TICK')
-        
-        else: #X
-            surface_code_circ.geo_append('H', s_coordinate)
-
-            for i in [d_1, d_2, d_3, d_4]:
-                if -1 < i[0] < col and -1 < i[1] < row:
-                    surface_code_circ.geo_append('CX', s_coordinate, i)
-
-            surface_code_circ.geo_append('H', s_coordinate)
-            surface_code_circ.geo_append('TICK')
-    
-    return surface_code_circ
 
