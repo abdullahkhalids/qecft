8212d2e105c6d83b9f6a4d004a4385dbd73ed816
victor-onofre <vonofre68@gmail.com>
2023-06-11 06:49:35 -0700

    add comments and clean code

--- drafts/_color_codes_syndrome_measurements.py
+++ drafts/_color_codes_syndrome_measurements.py
@@ -2,15 +2,25 @@
 #Licensed under GPLV3
 
 import stac
-import bidict
 import networkx as nx
 
 
 def vertices_physical_qubits(N):
+    '''
+    Creates graph for coordinates of the vertices/physical qubits with 4.8.8 color code family
+
+    Parameter
+    -----------
+    N : distance
+
+    Return
+    --------
+    H : Graph for coordinates of the vertices/physical qubits
+    faces: Faces of the graph
+    '''
     # Number of qubits in the bottom row
     # Note that the distance of each graph is equiavalent to the number of vertices in the bottom row.
     # We also have N total rows as well.
-    #N = 3
 
     # create a graph
     H = nx.Graph()
@@ -25,22 +35,15 @@ def vertices_physical_qubits(N):
     pl = dict()
 
     # heights of each column
-    # this only correct for N=3, 7, 11... It is incorrect for N=5,9,... Correct it.
     first_twenty_odd = list(range(1, 20, 2))
-    new = []
-    for i in range(0, len(first_twenty_odd), 2):
-        new.append(first_twenty_odd[i])
+    new_odd = [first_twenty_odd[i] for i in range(0, len(first_twenty_odd), 2)]
     # heights of each column
-    # this only correct for N=3, 7, 11... It is incorrect for N=5,9,... Correct it.
-    if N not in new:
+    if N not in new_odd:
+        # Case for N=3, 7, 11...
         heights = [i for i in range(1, N + 1, 2)] + [i for i in range(N, 2, -2)]
     else:
+        # Case for N=5,9,...
         heights = [i for i in range(3, N + 1, 2)] + [i for i in range(N, 0, -2)]
-    # print(heights)
-    # print([i for i in range(1,N+1,2)])
-    # print( [i for i in range(N, 2, -2)])
-    # For N = 5, we we would have this
-    # heights = [3, 5, 5, 3, 1]
 
     # So lets go column by column
     # [x, y] is the coordinate of the vertex
@@ -71,25 +74,26 @@ def vertices_physical_qubits(N):
     # add diagonal edges
     # Again, I wrote this for N=3,7,11.. It fails for N=5,9.. Correct it.
     for y in range(N):
-        if N not in new:
+        # Case for N=3, 7, 11...
+        if N not in new_odd:
             # left diagonal edges
             if y % 4 == 0:
                 H.add_edge(row_wise[y][0], row_wise[y+2][0])
-        # right diagonl edges
+            # right diagonl edges
             elif y % 2 == 0 and y < N-1:
                 H.add_edge(row_wise[y][-1], row_wise[y+2][-1])
         else:
-         # left diagonal edges
+            # Case for N=5,9,...
+            # left diagonal edges
             if y % 2 == 0 and y < N-1:
                 H.add_edge(row_wise[y][0], row_wise[y+2][0])
-        # right diagonl edges
+            # right diagonal edges
             elif y % 3 == 0:
                 print(y)
                 H.add_edge(row_wise[y-1][-1], row_wise[y-3][-1])
     # now draw it, giving networkx the positions of each vertex as well
     #nx.draw(H, pos=pl)
-    # print([i for i in range(1,N+1,2)])
-    # print( [i for i in range(N, 2, -2)])
+
     # First networkx has to check that the graph is indeed planar.
     # the planar embedding (pe) is its record of this.
     _, pe = nx.check_planarity(H)
@@ -111,8 +115,6 @@ def vertices_physical_qubits(N):
                 F.add(tuple(sorted(f)))
         else:
             if len(f) <= 4:
-                # before adding we are going to sort the edges and then turn the list into
-                # a tuple so it can be added to the set.
                 F.add(tuple(sorted(f)))
     # turn the faces set to a list and sort it.
     # not really necessary but nice.
@@ -123,53 +125,61 @@ def vertices_physical_qubits(N):
 
 def _color_codes_syndrome_measurements(d):
     '''
-    This function creates syndrome measurement circuit with 4.8.8 color code family
+    Creates syndrome measurement circuit with 4.8.8 color code family
 
     Parameter
     -----------
-    d :
-        distance
+    d : distance
 
     Return
     --------
     stac.Circuit with appended gates
     '''
+
+    # Retrieve graph for coordinates of the vertices/physical qubits and
+    # the respective faces of the graph given the distance d
     faces, H = vertices_physical_qubits(d)
 
+    # Define circuit for syndrome measurement
     circ = stac.Circuit()
+    # Data qubits
     circ.append_register(stac.QubitRegister('d', 0, len(H.nodes)))
+    # Subregister for ancilla qubits
     circ.append_register(stac.RegisterRegister('s', 0))
+    # Subregister for Z stabilizers ancilla qubits
     circ.register[0, 1].append(stac.RegisterRegister('t', 0))
+    # Subregister for X stabilizers ancilla qubits
     circ.register[0, 1].append(stac.RegisterRegister('t', 1))
 
+    # Define number of ancilla qubits in each subregister depending on the face 4 or 8
     for i in range(len(faces)):
         if len(faces[i]) == 4:
-            circ.register[0, 1][0].append(stac.QubitRegister('s', i, 1))
-            circ.register[0, 1][1].append(stac.QubitRegister('s', i, 1))
+            circ.register[0, 1][0].append(stac.QubitRegister('f', i, 1))
+            circ.register[0, 1][1].append(stac.QubitRegister('f', i, 1))
         else:
-            circ.register[0, 1][0].append(stac.QubitRegister('s', i, 5))
-            circ.register[0, 1][1].append(stac.QubitRegister('s', i, 5))
+            circ.register[0, 1][0].append(stac.QubitRegister('f', i, 5))
+            circ.register[0, 1][1].append(stac.QubitRegister('f', i, 5))
 
+    # Total number of data qubits
     qubit_number = list(H.nodes)
 
     # 4 qubit Z stabilizer
     for i in range(len(faces)):
         if len(faces[i]) == 4:
             for j in range(len(faces[i])):
-                # print(qubit_number.index(faces[i][j]), faces[i][j])
                 number = qubit_number.index(faces[i][j])
+                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
                 circ.append('CX', (0, 0, number), (0, 1, 0, i, 0))
             circ.append('MR', (0, 1, 0, i, 0))
             circ.append('TICK')
 
     # 4 qubit X stabilizer
     for i in range(len(faces)):
-        # print(i)
         if len(faces[i]) == 4:
             circ.append('H', (0, 1, 1, i, 0))
             for j in range(len(faces[i])):
-                # print(qubit_number.index(faces[i][j]), faces[i][j])
                 number = qubit_number.index(faces[i][j])
+                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
                 circ.append('CX', (0, 1, 1, i, 0), (0, 0, number))
             circ.append('H', (0, 1, 1, i, 0))
             circ.append('MR', (0, 1, 1, i, 0))
@@ -178,6 +188,7 @@ def _color_codes_syndrome_measurements(d):
     # Preparation of a 4 qubit cat state for the 8 qubit X stabibilizer
     for i in range(len(faces)):
         if len(faces[i]) == 8:
+            # (level, syndrome, type, face, i) = (0, 1, t, f, i)
             circ.append('H', (0, 1, 0, i, 0))
             circ.append('CX', (0, 1, 0, i, 0), (0, 1, 0, i, 1))
             circ.append('CX', (0, 1, 0, i, 0), (0, 1, 0, i, 2))
@@ -191,6 +202,7 @@ def _color_codes_syndrome_measurements(d):
     # Preparation of a 4 qubit cat state for the 8 qubit Z stabibilizer
     for i in range(len(faces)):
         if len(faces[i]) == 8:
+            # (level, syndrome, type, face, i) = (0, 1, t, f, i)
             circ.append('H', (0, 1, 1, i, 0))
             circ.append('CX', (0, 1, 1, i, 0), (0, 1, 1, i, 1))
             circ.append('CX', (0, 1, 1, i, 0), (0, 1, 1, i, 2))
@@ -206,14 +218,11 @@ def _color_codes_syndrome_measurements(d):
         if len(faces[i]) == 8:
             counter_x = 1
             for qubits in range(0, len(faces[i]), 2):
-                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
-                # print(qubit_number.index(faces[i][j]), faces[i][j])
                 number1 = qubit_number.index(faces[i][qubits])
                 number2 = qubit_number.index(faces[i][qubits + 1])
+                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
                 circ.append('CX', (0, 1, 1, i, counter_x), (0, 0, number1))
                 circ.append('CX', (0, 1, 1, i, counter_x), (0, 0, number2))
-                print(number1, counter_x)
-                print(number2, counter_x)
                 counter_x = counter_x + 1
             for k in range(1, 5):
                 circ.append('H', (0, 1, 1, i, k))
@@ -226,16 +235,12 @@ def _color_codes_syndrome_measurements(d):
             counter_z = 1
             for k in range(1, 5):
                 circ.append('H', (0, 1, 0, i, k))
-
             for qubits in range(0, len(faces[i]), 2):
-                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
-                # print(qubit_number.index(faces[i][j]), faces[i][j])
                 number1 = qubit_number.index(faces[i][qubits])
                 number2 = qubit_number.index(faces[i][qubits + 1])
+                # (level, syndrome, type, face, i) = (0, 1, t, f, i)
                 circ.append('CX', (0, 0, number1), (0, 1, 0, i, counter_z))
                 circ.append('CX', (0, 0, number2), (0, 1, 0, i, counter_z))
-                print(number1, counter_z)
-                print(number2, counter_z)
                 counter_z = counter_z + 1
 
             for k in range(1, 5):
